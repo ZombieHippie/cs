861eb93edcfd7b56e5868ce31bb7b8dfc6470d1d
Cole R Lawrence <msgzht@gmail.com>
2016-12-01 10:23:49 -0600

    adjust for performance and dpr

--- docs/source.js
+++ docs/source.js
@@ -10,8 +10,10 @@ fadeIn()
 function init() {
   container = document.getElementById('container')
   startTime = Date.now()
+
   camera = new THREE.Camera()
   camera.position.z = 1
+
   scene = new THREE.Scene()
 
   var geometry = new THREE.PlaneBufferGeometry(2, 2)
@@ -31,26 +33,61 @@ function init() {
   scene.add(mesh)
   renderer = new THREE.WebGLRenderer()
   container.appendChild(renderer.domElement)
-  onWindowResize()
   window.addEventListener('resize', onWindowResize, false)
+  onWindowResize()
 }
 
-function onWindowResize(event) {
+function onWindowResize(_event) {
+  wrWidth = window.innerWidth
+  wrHeight = window.innerHeight
   uniforms.iResolution.value.x = window.innerWidth
   uniforms.iResolution.value.y = window.innerHeight
-  renderer.setSize(window.innerWidth, window.innerHeight)
-}
-function animate() {
-  requestAnimationFrame(animate)
-  render()
+  renderer.setSize(wrWidth, wrHeight)
 }
 
-function render() {
-  var currentTime = Date.now()
-  uniforms.iGlobalTime.value = (currentTime - startTime) * 0.0001
+var diffTime;
+var cycle_at = 120;
+// Calculate this since multiplication is much faster than multiplication 
+var cycle_at_r = 1 / cycle_at;
+var cyclei = 0;
+var is_adjusted = false;
+// Watching performance and adjusting DPR in response.
+function time(timeNow, lastTime) {
+  if (is_adjusted) return
+  diffTime += timeNow - lastTime
+  if (cyclei++ > cycle_at) {
+    var avgTime = diffTime * cycle_at_r
+    diffTime = cyclei = 0
+    if (avgTime && avgTime > 30) {
+      renderer.setPixelRatio(1)
+      is_adjusted = true
+      console.warn("Reducing resolution of Three for your device.")
+    }
+  }
+}
+
+var currentTime;
+var nextTime;
+function animate() {
+  requestAnimationFrame(animate)
+  nextTime = Date.now()
+  // record for metrics
+  time(nextTime, currentTime)
+  currentTime = nextTime
+  render(currentTime - startTime)
+}
+
+function render(t) {
+  uniforms.iGlobalTime.value = t * 0.0001
   renderer.render(scene, camera)
 }
 
 function fadeIn() {
-  setTimeout(function () { container.style.opacity = 1 }, 100)
+  setTimeout(function () {
+    container.style.opacity = 1;
+    onWindowResize()
+    setTimeout(function () {
+      renderer.setPixelRatio(window.devicePixelRatio ? window.devicePixelRatio : 1)
+    }, 5000)
+  }, 100)
 }
